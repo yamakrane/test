3118d733f620c46becd1993a8a8ec97225e77408
Youssef Amakrane <youssef@stagemaroc.com>
2023-04-04 21:55:53 +0000

    issue with fileupload

--- app/Http/Livewire/Youssef.php
+++ app/Http/Livewire/Youssef.php
@@ -13,29 +13,20 @@ class Youssef extends Component implements HasForms
 {
     use InteractsWithForms;
 
+    public $user;
+
+    public function mount(){
+        
+        $this->user = Auth::user();
+
+        $this->form->fill([
+            'cv' => $this->user->cv
+        ]);
+    }
+
     public function getFormSchema(){
         return [
-            Radio::make('tst')->options([
-                0 => 'Test',
-                1 => 'Test1',
-                2 => 'Test2',
-                3 => 'Tes3t',
-            ])->default(1),
-
-            Select::make('test')->options([
-                0 => 'Test',
-                1 => 'Test1',
-                2 => 'Test2',
-                3 => 'Tes3t',
-            ])->default(3)->searchable()->preload(),
-
-            CheckboxList::make('tstx')->options([
-                'test' => 'Test',
-                'test1' => 'Test1',
-                'test2' => 'Test2',
-                'test3' => 'Tes3t',
-            ])->default(['test1','test2'])
-
+            FileUpload::make('cv')->required()
         ];
     }
 
--- resources/views/livewire/youssef.blade.php
+++ resources/views/livewire/youssef.blade.php
@@ -1,3 +1,21 @@
 <div>
-    {{ $this->form }}
+    <button type="button" wire:click="$toggle('openModal')">Open Modal</button>
+
+    <x-dialog-modal wire:model="openModal" id="submissionModal">
+            <x-slot name="title">
+                {{ __('Apply to this job') }}
+            </x-slot>
+
+            <x-slot name="content">
+                    {{ $this->form }}
+            </x-slot>
+
+            <x-slot name="footer">
+                <div class="flex space-x-2">
+                    <x-buttons.secondary wire:click="$toggle('openModal')" wire:loading.attr="disabled">
+                        {{ __('Cancel') }}
+                    </x-buttons.secondary>
+                </div>
+            </x-slot>
+    </x-dialog-modal>
 </div>
